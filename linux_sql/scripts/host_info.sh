0d765e10bc9440f15f7a74a5d77c44e9c28feeda
Ajithan Urutheran <urutheranajithan@gmail.com>
2024-01-25 02:43:48 +0000

    Adding executable permissions to files

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+#CLI Arguement
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
-
+#Validate Arguement
 if [ "$#" -ne 5 ]; then
     echo "Illegal number of parameters"
     exit 1
 fi
 
+#Retrieve Hardware Spec
 lscpu_out=`lscpu`
 df_out=`df -BM /`
 hostname=$(hostname -f)
@@ -25,10 +27,10 @@ l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | xargs | s
 total_mem=$(echo "$vmstat_mb" | tail -1 | awk '{print $4}')
 timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
+#Executes Statements
 export PGPASSWORD=$psql_password
-
 insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, \"timestamp\", total_mem) VALUES ('$hostname','$cpu_number','$cpu_architecture','$cpu_model','$cpu_mhz','$l2_cache','$timestamp','$total_mem');"
 
-
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
 exit $?
