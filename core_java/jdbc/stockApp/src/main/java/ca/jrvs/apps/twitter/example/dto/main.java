7353f982de599d681a2114f2568624f6ea6b5d88
Ajithan Urutheran <ajithanurutheran@gmail.com>
2024-03-12 18:43:57 -0400

    Day 3: Adding code for HTTP & JSON

---
+++ core_java/jdbc/stockApp/src/main/java/ca/jrvs/apps/twitter/example/dto/main.java
@@ -0,0 +1,36 @@
+package ca.jrvs.apps.twitter.example.dto;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+public class main {
+    public static void main(String[] args) {
+
+        String symbol = "AAPL";
+        String apiKey = "1554b33734msh65466f6d5c8faeap189293jsne651b15f82ac";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+symbol+"&datatype=json"))
+                .header("X-RapidAPI-Key", apiKey)
+                .header("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
