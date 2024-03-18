47efaea947a77800a0437b0512466814f6ec905a
Ajithan Urutheran <urutheranajithan@gmail.com>
2024-03-18 20:00:26 +0000

    Finished PositionDao and QuoteDAO. Implementing testing next

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/PositionDao.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/PositionDao.java
@@ -5,47 +5,135 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Optional;
 
 public class PositionDao implements CrudDao<Position, String> {
 
     private Connection c;
     private static final String GET_ONE = "Select symbol, number_of_shares, value_paid FROM position WHERE symbol=?";
+    private static final String GET_ALL = "SELECT symbol, number_of_shares, value_paid FROM position";
+    private static final String DELETE_ONE = "DELETE FROM position WHERE symbol=?";
+    private static final String DELETE_ALL = "DELETE FROM position";
+    private static final String INSERT_ONE = "INSERT into position (symbol, number_of_shares, value_paid) VALUES (?,?,?)";
+    private static final String UPDATE_ONE = "UPDATE position SET position number_of_shares = ?, value_paid = ? WHERE symbol = ?";
     public PositionDao(Connection c){
         this.c = c;
     }
 
     @Override
     public Position save(Position entity) throws IllegalArgumentException {
-        return null;
+        if(entity == null){
+            throw new IllegalArgumentException("Null position");
+        }
+
+        if(this.findById(entity.getTicker()).isEmpty()){
+            try(PreparedStatement statement = this.c.prepareStatement(INSERT_ONE);){
+                statement.setString(1,entity.getTicker());
+                statement.setInt(2,entity.getNumOfShares());
+                statement.setDouble(3,entity.getValuePaid());
+
+                statement.executeUpdate();
+                return entity;
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                throw new RuntimeException("Failed to insert position");
+            }
+        }
+        else{
+            try(PreparedStatement statement = this.c.prepareStatement(UPDATE_ONE);){
+                statement.setInt(1,entity.getNumOfShares());
+                statement.setString(3,entity.getTicker());
+                statement.setDouble(2,entity.getValuePaid());
+
+                statement.executeUpdate();
+                return entity;
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                throw new RuntimeException("Failed to update specified ID");
+            }
+        }
+
     }
 
     @Override
     public Optional<Position> findById(String s) throws IllegalArgumentException {
-        Position position = new Position();
+
+        if (s == null){
+            throw new IllegalArgumentException("Null ID Detected");
+        }
         try(PreparedStatement statement = this.c.prepareStatement(GET_ONE);){
             statement.setString(1,s);
             ResultSet rs = statement.executeQuery();
-            
+            if (rs.next()){
+                return Optional.ofNullable(createPosition(rs));
+            }
         }
         catch(SQLException e){
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+        return Optional.empty();
     }
 
     @Override
     public Iterable<Position> findAll() {
-        return null;
+        ArrayList<Position> positionList = new ArrayList<>();
+
+        try(PreparedStatement statement = this.c.prepareStatement(GET_ALL);){
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                positionList.add(createPosition(rs));
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return positionList;
     }
 
     @Override
     public void deleteById(String s) throws IllegalArgumentException {
-
+        if(s == null){
+            throw new IllegalArgumentException("Null ID Detected");
+        }
+        try(PreparedStatement statement = this.c.prepareStatement(DELETE_ONE);){
+            statement.setString(1,s);
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to delete specified ID");
+        }
     }
 
     @Override
     public void deleteAll() {
+        try(PreparedStatement statement = this.c.prepareStatement(DELETE_ALL);){
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to delete all IDs");
+        }
+    }
 
+    /**
+     *
+     * @param rs
+     * @return position - Can be optional
+     * @throws SQLException
+     */
+    private Position createPosition(ResultSet rs) throws SQLException{
+        Position position = new Position();
+
+        position.setTicker(rs.getString("symbol"));
+        position.setValuePaid(rs.getDouble("value_paid"));
+        position.setNumOfShares(rs.getInt("number_of_shares"));
+
+        return position;
     }
 }
--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/QuoteDao.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/QuoteDao.java
@@ -2,34 +2,154 @@ package ca.jrvs.apps.stockquote.dao;
 
 import java.sql.Connection;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Optional;
 
 public class QuoteDao implements CrudDao<Quote, String> {
 
     private Connection c;
-
+    private static final String GET_ONE = "Select * FROM quote WHERE symbol=?";
+    private static final String GET_ALL = "Select * from quote";
+    private static final String DELETE_ONE = "DELETE FROM quote WHERE symbol=?";
+    private static final String DELETE_ALL = "DELETE FROM quote";
+    private static final String INSERT_ONE = "INSERT into quote (symbol, open, high, low, price, volume, latest_trading_day, previous_close, change, change_percent, timestamp) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+    private static final String UPDATE_ONE = "UPDATE quote SET open = ?, high = ?, low = ?, price = ?, volume = ?, latest_trading_day = ?, previous_close = ?, change = ?, change_percent = ?, timestamp = ? WHERE symbol = ?";
     @Override
     public Quote save(Quote entity) throws IllegalArgumentException {
-        return null;
+        if(entity == null){
+            throw new IllegalArgumentException("Null Quote");
+        }
+
+        if(this.findById(entity.getTicker()).isEmpty()){
+            try(PreparedStatement statement = this.c.prepareStatement(INSERT_ONE);){
+                statement.setString(1, entity.getTicker());
+                statement.setDouble(2, entity.getOpen());
+                statement.setDouble(3, entity.getHigh());
+                statement.setDouble(4, entity.getLow());
+                statement.setDouble(5, entity.getPrice());
+                statement.setInt(6, entity.getVolume());
+                statement.setDate(7, entity.getLatestTradingDay());
+                statement.setDouble(8, entity.getPreviousClose());
+                statement.setDouble(9, entity.getChange());
+                statement.setString(10, entity.getChangePercent());
+                statement.setTimestamp(11,entity.getTimestamp());
+
+
+                statement.executeUpdate();
+                return entity;
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                throw new RuntimeException("Failed to insert quote");
+            }
+        }
+        else{
+            try(PreparedStatement statement = this.c.prepareStatement(UPDATE_ONE);){
+
+                statement.setDouble(1, entity.getOpen());
+                statement.setDouble(2, entity.getHigh());
+                statement.setDouble(3, entity.getLow());
+                statement.setDouble(4, entity.getPrice());
+                statement.setInt(5, entity.getVolume());
+                statement.setDate(6, entity.getLatestTradingDay());
+                statement.setDouble(7, entity.getPreviousClose());
+                statement.setDouble(8, entity.getChange());
+                statement.setString(9, entity.getChangePercent());
+                statement.setTimestamp(10,entity.getTimestamp());
+                statement.setString(11, entity.getTicker());
+
+                statement.executeUpdate();
+                return entity;
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                throw new RuntimeException("Failed to update specified ID");
+            }
+        }
     }
 
     @Override
     public Optional<Quote> findById(String s) throws IllegalArgumentException {
+        if (s == null){
+            throw new IllegalArgumentException("Null ID Detected");
+        }
+        try(PreparedStatement statement = this.c.prepareStatement(GET_ONE);){
+            statement.setString(1,s);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()){
+                return Optional.ofNullable(createQuote(rs));
+            }
+
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
         return Optional.empty();
     }
 
     @Override
     public Iterable<Quote> findAll() {
-        return null;
+        ArrayList<Quote> quoteList = new ArrayList<>();
+
+        try(PreparedStatement statement = this.c.prepareStatement(GET_ALL);){
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                quoteList.add(createQuote(rs));
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return quoteList;
     }
 
     @Override
     public void deleteById(String s) throws IllegalArgumentException {
-
+        if(s == null){
+            throw new IllegalArgumentException("Null ID Detected");
+        }
+        try(PreparedStatement statement = this.c.prepareStatement(DELETE_ONE);){
+            statement.setString(1,s);
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to delete specified ID");
+        }
     }
+
 
     @Override
     public void deleteAll() {
+        try(PreparedStatement statement = this.c.prepareStatement(DELETE_ALL);){
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to delete all IDs");
+        }
+    }
 
+    private Quote createQuote(ResultSet rs) throws SQLException{
+        Quote quote = new Quote();
+
+        quote.setTicker(rs.getString("symbol"));
+        quote.setOpen(rs.getDouble("open"));
+        quote.setHigh(rs.getDouble("high"));
+        quote.setLow(rs.getDouble("low"));
+        quote.setPrice(rs.getDouble("price"));
+        quote.setVolume(rs.getInt("volume"));
+        quote.setLatestTradingDay(rs.getDate("latest_trading_day"));
+        quote.setPreviousClose(rs.getDouble("previous_close"));
+        quote.setChange(rs.getDouble("change"));
+        quote.setChangePercent(rs.getString("change_percent"));
+        quote.setTimestamp(rs.getTimestamp("timestamp"));
+
+        return quote;
     }
 }
